6448a52270bb41190707b7c3c947c9f143651098
GuruPrasadhBM <guruprasadh.bm@gmail.com>
2021-07-27 22:36:13 +0530

    Updates to train.py

--- src/train.py
+++ src/train.py
@@ -1,8 +1,22 @@
-import pandas as pd
-from sklearn import preprocessing   
 
-if __name__ == "__main__":
-    df = pd.read_csv(TRAINING_DATA)
-    train_df = df[df.kfold.isin(FOLD_MAPPING.get(FOLD))]
-    valid_df= df[df.kfold=FOLD]
-    
+import os
+import pandas as pd
+from sklearn import ensemble
+from sklearn import preprocessing
+from sklearn import metrics
+import joblib
+
+from . import dispatcher
+
+TRAINING_DATA = os.environ.get("TRAINING_DATA")
+TEST_DATA = os.environ.get("TEST_DATA")
+FOLD = int(os.environ.get("FOLD"))
+MODEL = os.environ.get("MODEL")
+
+FOLD_MAPPPING = {
+    0: [1, 2, 3, 4],
+    1: [0, 2, 3, 4],
+    2: [0, 1, 3, 4],
+    3: [0, 1, 2, 4],
+    4: [0, 1, 2, 3]
+}
