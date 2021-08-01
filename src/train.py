c7ef8137e6fad4e3a37c08ecc3b1ceb9b6898410
GuruPrasadhBM <guruprasadh.mahalingam@gmail.com>
2021-08-01 10:42:46 +0530

    KFold

--- src/train.py
+++ src/train.py
@@ -1,22 +1,13 @@
-
-import os
 import pandas as pd
-from sklearn import ensemble
 from sklearn import preprocessing
-from sklearn import metrics
-import joblib
 
-from . import dispatcher
+TRAINING_DATA = None
+FOLD = None
 
-TRAINING_DATA = os.environ.get("TRAINING_DATA")
-TEST_DATA = os.environ.get("TEST_DATA")
-FOLD = int(os.environ.get("FOLD"))
-MODEL = os.environ.get("MODEL")
-
-FOLD_MAPPPING = {
-    0: [1, 2, 3, 4],
-    1: [0, 2, 3, 4],
-    2: [0, 1, 3, 4],
-    3: [0, 1, 2, 4],
-    4: [0, 1, 2, 3]
+FOLD_MAPPING = {
+    0 : [1,2,3,4],
+    1: [0,2,3,4],
+    2:[0,1,3,4],
+    3:[0,1,2,4],
+    4:[0,1,2,3]
 }
